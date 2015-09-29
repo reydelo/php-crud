e4e4eb624549527af476549d9c76b7a90dae0d07
reydelo <reyna.deloge@gmail.com>
2015-09-28 23:42:00 -0600

    delete and vote for pencils

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 include_once 'database.php';
+
+// delete condition
+if(isset($_GET['delete'])) {
+  $sql_query = "DELETE FROM pencils WHERE id=".$_GET['delete'];
+  mysql_query($sql_query);
+  header("Location: $_SERVER[PHP_SELF]");
+}
+
+if(isset($_GET['vote'])) {
+  $sql_query = "UPDATE pencils SET vote_count=vote_count + 1 WHERE id=".$_GET['vote'];
+  mysql_query($sql_query);
+  header("Location: $_SERVER[PHP_SELF]");
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,8 +31,6 @@ include_once 'database.php';
     <div class="row">
 
       <a href="create.php"><button class="btn btn-warning">Nominate a Pencil!</button></a>
-
-
 
       <div class="col-md-6">
         <h2>Best Pencils</h2>
@@ -40,18 +51,17 @@ include_once 'database.php';
       {
           ?>
           <tr>
-            <th><span class="pull-right glyphicon glyphicon-star text-warning"><?php echo $row[2]; ?></span></th>
+            <th><?php echo $row[2]; ?> <a href="javascript:upVotePencil('<?php echo $row[3]; ?>')"> <span class="glyphicon glyphicon-thumbs-up text-warning"></span></a></th>
             <td><?php echo $row[0]; ?></td>
             <td><?php echo $row[1]; ?></td>
             <td class="text-right">
               <a href="javascript:editPencil('<?php echo $row[3]; ?>')"><button class="btn btn-primary"><span class="glyphicon glyphicon-cog"></span></button></a>
-              <button class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button>
+              <a href="javascript:deletePencil('<?php echo $row[3]; ?>')"><button class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button></a>
             </td>
           </tr>
           <?php
       }
       ?>
-
           </tbody>
         </table>
       </div>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 function editPencil(id) {
   window.location.href='update.php?edit='+id;
 }
+
+function deletePencil(id) {
+  window.location.href='index.php?delete='+id;
+}
+
+function upVotePencil(id) {
+  window.location.href='index.php?vote='+id;
+}
