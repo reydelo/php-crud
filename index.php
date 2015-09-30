d7ccd0dab595c7013a3709aef329c775a275d47b
reydelo <reyna.deloge@gmail.com>
2015-09-29 21:03:55 -0600

    remove deprecated mysql functions and replace with PDO

--- index.php
+++ index.php
@@ -25,53 +25,50 @@ if(isset($_GET['vote'])) {
 </head>
 <body>
 
-  <body>
+  <div class="container">
+    <div class="row">
 
-    <div class="container">
-      <div class="row">
+      <a href="create.php"><button class="btn btn-warning">Nominate a Pencil!</button></a>
 
-        <a href="create.php"><button class="btn btn-warning">Nominate a Pencil!</button></a>
-
-        <div class="col-md-6">
-          <h2>Best Pencils</h2>
-          <table class="table">
-            <thead>
-              <tr>
-                <th colspan="1"></th>
-                <th>Brand</th>
-                <th>Grade</th>
-                <th colspan="1"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              $sql_query="SELECT * FROM pencils ORDER BY vote_count DESC";
-              $result_set=mysql_query($sql_query);
-              while($row = mysql_fetch_array($result_set))
-              {
-                ?>
-                <tr>
-                  <th><?php echo $row[2]; ?> <a href="javascript:upVotePencil('<?php echo $row[3]; ?>')"> <span class="glyphicon glyphicon-thumbs-up text-warning"></span></a></th>
-                  <td><?php echo $row[0]; ?></td>
-                  <td><?php echo $row[1]; ?></td>
-                  <td class="text-right">
-                    <a href="javascript:editPencil('<?php echo $row[3]; ?>')"><button class="btn btn-primary"><span class="glyphicon glyphicon-cog"></span></button></a>
-                    <a href="javascript:deletePencil('<?php echo $row[3]; ?>')"><button class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button></a>
-                  </td>
-                </tr>
-                <?php
-              }
+      <div class="col-md-6">
+        <h2>Best Pencils</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th colspan="1"></th>
+              <th>Brand</th>
+              <th>Grade</th>
+              <th colspan="1"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $results = $db->query("SELECT * FROM pencils ORDER BY vote_count DESC");
+            // print_r($results->fetchAll(PDO::FETCH_ASSOC));
+            foreach ($results as $pencil) {
               ?>
-            </tbody>
-          </table>
-        </div>
-
+              <tr>
+                <th><?php echo $pencil["vote_count"]; ?> <a href="javascript:upVotePencil('<?php echo $pencil["id"]; ?>')"> <span class="glyphicon glyphicon-thumbs-up text-warning"></span></a></th>
+                <td><?php echo $pencil["brand"]; ?></td>
+                <td><?php echo $pencil["grade"]; ?></td>
+                <td class="text-right">
+                  <a href="javascript:editPencil('<?php echo $pencil["id"]; ?>')"><button class="btn btn-primary"><span class="glyphicon glyphicon-cog"></span></button></a>
+                  <a href="javascript:deletePencil('<?php echo $pencil["id"]; ?>')"><button class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button></a>
+                </td>
+              </tr>
+              <?php
+            }
+            ?>
+          </tbody>
+        </table>
       </div>
-    </div>
 
-    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>
-    <script type="text/javascript" src="js/main.js"></script>
-  </body>
-  </html>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>
+  <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
