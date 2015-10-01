f03eb44a0d62ae64e7f66c553e901ed47d59285d
reydelo <reyna.deloge@gmail.com>
2015-10-01 16:49:46 -0600

    make a RESTful API endpoint!!!!

---
+++ handlers/pencilManager.php
@@ -0,0 +1,72 @@
+<?php
+// include here logic to handle different types of requests in different manners
+require_once("../models/PencilModel.php");
+
+$dsn = "mysql:host=localhost;dbname=pencil_table;port=8889";
+$user = "root";
+$password = "root";
+
+$db = new PDO($dsn, $user, $password);
+$obj = new PencilModel($db);
+
+switch ($_SERVER['REQUEST_METHOD']) {
+
+  case "GET":
+
+    $id = explode("pencil/", $_SERVER['REQUEST_URI']);
+
+    if ($id[1] != ""){
+      /*
+       Query the database to get the information
+       about the pencil with ID = $id[1]
+      */
+
+      $result = $obj->get_pencil_by_id($id[1]);
+
+    }
+    else {
+
+      /*
+       Query the database to get the information
+       about all the pencils
+      */
+
+      $result = $obj->get_pencils();
+
+    }
+
+    break;
+
+  case "POST":
+
+    // Save a new record in the database
+
+    $result = $obj->register_new_pencil($_POST);
+
+    break;
+
+  case "PUT":
+
+    // Retrieve additional data
+    $result = $obj->loan_pencil($_POST, $id);
+
+    break;
+
+  case "DELETE":
+
+    $id = explode("pencil/", $_SERVER['REQUEST_URI']);
+
+    if (isset($id[1])){
+      $result = $obj->delete_pencil($id[1]);
+    }
+
+    break;
+
+}
+
+$json = json_encode($result);
+echo $json;
+
+return;
+
+ ?>
--- models/PencilModel.php
+++ models/PencilModel.php
@@ -8,8 +8,28 @@ class PencilModel
     $this->db = $db;
   }
 
-  public function getAllPencils() {
-    return $this->db->query('SELECT * FROM pencils');
+  public function get_pencils() {
+    $res = $this->db->query('SELECT * FROM pencils');
+    return $res->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function get_pencil_by_id($id) {
+    $res = $this->db->query('SELECT * FROM pencils WHERE id='.$id);
+    return $res->fetch(PDO::FETCH_ASSOC);
+  }
+
+  public function register_new_pencil($form_data) {
+    // the $form_data argument comes from the form method POST
+    // so $form_data['brand'] == req.body.brand
+    return $this->db->query('INSERT INTO pencils (brand, grade) VALUES ($form_data[0], $form_data[1])');
+  }
+
+  public function loan_pencil($form_data, $id) {
+    return $this->db->query('UPDATE pencils SET brand=$form_data[0] grade=$form_data[1] WHERE id='.$id);
+  }
+
+  public function delete_pencil($id) {
+    return $this->$db->query('DELETE FROM pencils WHERE id='.$id);
   }
 }
 
